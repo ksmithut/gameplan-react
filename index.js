f014de5752696188d78306a86aeb1b6a5731bb48
ksmithut <github@ksmithut.com>
2020-02-03 09:14:57 -0700

    simplify

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const path = require('path')
  */
 
 /**
- * @typedef {object.<string, OptionDefinition>} OptionDefinitions
+ * @typedef {{[key: string]: OptionDefinition}} OptionDefinitions
  */
 
 /**
@@ -26,23 +26,13 @@ exports.options = ({ directory }) => ({
     type: 'string',
     description: 'The name of the project',
     default: path.basename(directory)
-  },
-  typescript: {
-    type: 'boolean',
-    description: 'Make it a typescript project',
-    default: false
-  },
-  yarn: {
-    type: 'boolean',
-    description: 'Use yarn instead of npm',
-    default: true
   }
 })
 
 /**
  * @param {object} data
  * You'll want to change data.options to match what you have in your
- * @param {{ name: string, typescript: boolean, yarn: boolean }} data.options - The resolved options as defined from above
+ * @param {{ name: string }} data.options - The resolved options as defined from above
  * @param {object} data.operations
  * @param {(fromPath: string|string[], toPath: string|string[]) => void} data.operations.copy -
  *   Copy a file from fromPath (a relative path from the root of this repo) to
@@ -55,7 +45,7 @@ exports.options = ({ directory }) => ({
  * @param {(object: object, toPath: string|string[]) => void} data.operations.json -
  *   Render the raw object to the toPath (A relative path from the root of the
  *   destination directory)
- * @param {(command: string, ...args: string|string[]) => void} data.operations.spawn -
+ * @param {(command: string, ...args: (string|string[])[]) => void} data.operations.spawn -
  *   Run the command in a spawned process
  */
 exports.run = ({ options, operations }) => {
@@ -88,14 +78,8 @@ exports.run = ({ options, operations }) => {
   packageJSON.homepage = '.'
 
   operations.copy(['templates', '.browserslistrc'], ['.browserslistrc'])
-  operations.copy(
-    ['templates', 'src', options.typescript ? 'index.js' : 'index.js'],
-    ['src', options.typescript ? 'index.tsx' : 'index.js']
-  )
-  operations.copy(
-    ['templates', 'src', options.typescript ? 'App.js' : 'App.js'],
-    ['src', options.typescript ? 'App.tsx' : 'App.js']
-  )
+  operations.copy(['templates', 'src', 'index.js'], ['src', 'index.js'])
+  operations.copy(['templates', 'src', 'App.js'], ['src', 'App.js'])
   const publicFiles = ['favicon.ico', 'index.html', 'manifest.json']
   publicFiles.forEach(file => {
     operations.copy(['templates', 'public', file], ['public', file])
@@ -106,23 +90,17 @@ exports.run = ({ options, operations }) => {
     .add('react')
     .add('react-dom')
 
-  if (options.typescript) {
-    operations.copy(
-      ['templates', 'src', 'react-app-env.d.ts'],
-      ['src', 'react-app-env.d.ts']
-    )
-    operations.copy(['templates', 'tsconfig.json'], ['tsconfig.json'])
-    devDependencies.add('@types/jest')
-    dependencies
-      .add('@types/node')
-      .add('@types/react')
-      .add('@types/react-dom')
-      .add('typescript')
-  }
+  operations.copy(['templates', 'tsconfig.json'], ['tsconfig.json'])
+  devDependencies.add('@types/jest')
+  devDependencies
+    .add('@types/node')
+    .add('@types/react')
+    .add('@types/react-dom')
+    .add('typescript')
 
   // Linting
   {
-    const ext = options.typescript ? 'ts' : 'js'
+    const ext = 'js'
     const src = `'src/**/*.${ext}'`
     packageJSON.scripts.format = `prettier-eslint ${src} --write`
     packageJSON.scripts.lint = `eslint ${src} && prettier-eslint ${src} --list-different`
@@ -136,23 +114,15 @@ exports.run = ({ options, operations }) => {
       .add('eslint-plugin-standard')
       .add('eslint-plugin-react')
       .add('prettier-eslint-cli')
-    if (options.typescript) devDependencies.add('@typescript-eslint/parser')
-    operations.copy(
-      ['templates', options.typescript ? '.eslintrc.typescript' : '.eslintrc'],
-      ['.eslintrc']
-    )
+    operations.copy(['templates', '.eslintrc'], ['.eslintrc'])
   }
 
   operations.spawn('git', ['init'])
   operations.json(packageJSON, ['package.json'])
   operations.copy(['templates', '.gitignore'], ['.gitignore'])
 
-  const installCommand = options.yarn ? 'yarn' : 'npm'
-  const installArg = options.yarn ? 'add' : 'install'
-  const devFlag = options.yarn ? '--dev' : '--save-dev'
-  const baseArgs = [installArg]
-  const devArgs = baseArgs.concat([devFlag, ...devDependencies])
-  const prodArgs = baseArgs.concat([...dependencies])
-  if (dependencies.size) operations.spawn(installCommand, prodArgs)
-  if (devDependencies.size) operations.spawn(installCommand, devArgs)
+  if (dependencies.size) operations.spawn('yarn', ['add', ...dependencies])
+  if (devDependencies.size) {
+    operations.spawn('yarn', ['add', '--dev', ...devDependencies])
+  }
 }
